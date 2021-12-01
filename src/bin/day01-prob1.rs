fa12b5e38f07659b680f2a56d63badc45de76722
Chris Lieb <chris.lieb@gmail.com>
2021-12-01 09:02:31 -0500

    Add solution for day 1, problem 1

--- src/bin/day01-prob1.rs
+++ src/bin/day01-prob1.rs
@@ -1,5 +1,22 @@
-fn main() {
+use itertools::Itertools;
 
+fn main() {
+    let depths = std::fs::read_to_string("src/bin/day01.txt")
+        .map(|file| {
+            file.lines()
+                .filter(|line| !line.is_empty())
+                .map(|val| val.parse::<u16>().ok().unwrap())
+                .collect::<Vec<u16>>()
+        })
+        .expect("Unable to open file");
+    println!(
+        "{:?}",
+        depth_increases(depths)
+    );
+}
+
+fn depth_increases(depths: Vec<u16>) -> usize {
+    depths.iter().tuple_windows().filter(|(p, n)| n > p).count()
 }
 
 #[cfg(test)]
@@ -8,6 +25,6 @@ mod test {
 
     #[test]
     fn test1() {
-
+        assert_eq!(depth_increases(vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]), 7)
     }
 }
